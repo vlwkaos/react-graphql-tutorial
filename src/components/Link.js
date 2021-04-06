37885e5bb6c4d4d466bbc264eda42ee314c71846
vlwkaos <mgparkprint@gmail.com>
2021-04-06 20:12:19 +0900

    Add Prisma Server / Add Mock Link List

---
+++ src/components/Link.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Link = (props) => {
+    const { link } = props;
+    return (
+        <div>
+            <div> {link.description} ({link.url}) </div>
+        </div>
+    );
+};
+
+export default Link;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
+import {
+  ApolloClient, ApolloProvider,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+import './styles/index.css';
+
+// 서버 주소
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+// 주소를 주고 cache 만들고 ApolloClient 초기화
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
